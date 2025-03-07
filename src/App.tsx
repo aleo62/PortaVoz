8c413f89b82ad6ed102d30d090e253491f5aaca4
aleo62 <aleiwamoto07@gmail.com>
2025-03-07 07:45:54 -0300

    Projeto criado com sucesso!

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+        <div className="text-7xl h-[100vh] font-bold flex items-center justify-center">
+            Project Created!
+        </div>
+    </>
+  )
+}
+
+export default App
