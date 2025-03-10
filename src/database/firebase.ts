06a8b466b7a66de3a6b341632b95034fd0489e10
aleo62 <aleiwamoto07@gmail.com>
2025-03-10 19:25:12 -0300

    Erros Vercel e Firebase arrumados!

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -6,8 +6,6 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 
-console.log(import.meta.env.VITE_API_LEAD)
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_LEAD,
     authDomain: import.meta.env.VITE_DOMAIN,
