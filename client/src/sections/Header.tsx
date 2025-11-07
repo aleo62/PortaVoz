767bd607ee774dc990b5ac875d5c9ab15196f69a
aleo62 <aleiwamoto07@gmail.com>
2025-11-07 17:14:49 -0300

    Erro solucionado

--- client/src/sections/Header.tsx
+++ client/src/sections/Header.tsx
@@ -12,8 +12,6 @@ export const Header = () => {
     const [hidden, setHidden] = useState(false);
     const [outside, setOutside] = useState(false);
     const [lastScrollY, setLastScrollY] = useState(0);
-
-    const [isSearchOpen, setIsSearchOpen] = useState(false);
     const { isDarkTheme, setIsDarkTheme } = useTheme();
 
     useEffect(() => {
@@ -40,26 +38,23 @@ export const Header = () => {
 
     return (
         <>
-            <SearchModal isOpen={isSearchOpen} onClose={() => setIsSearchOpen(false)} />
+            
 
             <header
                 className={`header fixed top-0 left-0 z-40 flex h-fit w-full items-center transition-[translate] duration-300 ${hidden ? "-translate-y-[290%]" : "translate-y-0"} ${outside ? "bg-body-background shadow-[0px_4px_46px_-16px_rgba(0,_0,_0,_0.1)]" : "bg-transparent"}`}
             >
                 <div className="mx-auto flex w-full max-w-screen-2xl justify-between px-4 py-2 lg:grid lg:grid-cols-[1fr_4fr] lg:gap-4 lg:px-8">
-                    {/* LOGO */}
                     <h1 className="font-title flex items-center font-bold">
                         <a href="/" className="logo">
                             <img src={portaVozLogo(isDarkTheme)} width={90} alt="PortaVoz" />
                         </a>
                     </h1>
 
-                    {/* NAVBAR */}
                     <div className="flex flex-row-reverse items-center justify-center lg:grid lg:grid-cols-[3fr_1fr]">
                         <div className="relative pl-3 max-lg:flex max-lg:items-center lg:pl-0">
                             <Navbar />
                         </div>
 
-                        {/* BUTTONS AND SEARCH/LANGUAGE */}
                         <div className="flex items-center justify-end">
                             <div className="relative flex items-center gap-4 border-r-2 border-zinc-200 pr-4 dark:border-zinc-700">
                                 <button
